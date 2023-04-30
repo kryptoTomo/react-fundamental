859086de2fdaf125314dcc2b5c1c65704eca60d7
kryptoTomo <tomasz.madej101@gmail.com>
2023-04-30 16:42:34 +0200

    updating state that depends on the previous state

--- react-app/src/components/NewExpense/ExpenseForm.js
+++ react-app/src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
@@ -10,25 +10,31 @@ const ExpenseForm = () => {
   });
 
   const titleChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredTitle: event.target.value,
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredTitle: event.target.value,
+      };
     });
     console.log(event.target.value);
   };
 
   const amountChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredAmount: event.target.value,
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredAmount: event.target.value,
+      };
     });
     console.log(event.target.value);
   };
 
   const dateChangeHandler = (event) => {
-    setUserInput({
-      ...userInput,
-      enteredDate: event.target.value,
+    setUserInput((prevState) => {
+      return {
+        ...prevState,
+        enteredDate: event.target.value,
+      };
     });
     console.log(event.target.value);
   };
